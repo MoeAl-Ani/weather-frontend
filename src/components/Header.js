4c15be897ab9e58ded71988266da7413b4f554b2
Mohammed Al-Ani <mohammedalanny@gmail.com>
2021-06-18 19:02:16 +0300

    - removed google fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const Header = ({title}) => {
     return (
-        //<header style={headerStyle}>
         <header className='header'>
             <h1>{title}</h1>
         </header>
